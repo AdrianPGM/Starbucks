fdedde1f0194cc813631d45fd2a5334225889582
AdrianPGM <adrianmatheuspgm@gmail.com>
2024-12-20 14:43:28 -0300

    created_stabucks

---
+++ starbucks-main/script.js
@@ -0,0 +1,11 @@
+let image = document.querySelector('.starbucks')
+let circle = document.querySelector('.circle')
+
+
+function rootImage(src) {
+    image.src = src
+}
+
+function circleBackground(color){
+    circle.style.background = color
+}   
